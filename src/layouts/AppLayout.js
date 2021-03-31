db3df73c59d84f91b6c786d8b17e354466d556fc
brilingas <lauris652@gmail.com>
2021-03-31 21:11:18 +0300

    Fix imports

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
 import Home from '../views/Home';
+import Sidebar from '../components/Sidebar/Sidebar';
 
 const AppLayout = ({ children }) => {
   const [signupModalActive, setSignupModalActive] = useState(false);
@@ -10,6 +11,16 @@ const AppLayout = ({ children }) => {
   return (
     <>
       <main className="site-content">
+      <Sidebar
+        routes={routes}
+        logoText={"Creative Tim"}
+        logo={logo}
+        image={image}
+        handleDrawerToggle={handleDrawerToggle}
+        open={mobileOpen}
+        color={color}
+        {...rest}
+      />
       {children}
       </main>
     </>
