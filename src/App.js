53f315771febbadbdaf711631848c603daebbe1f
brilingas <lauris652@gmail.com>
2021-04-02 00:31:56 +0300

    Working sidebar navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { useRef, useEffect } from 'react';
-import { Switch, useLocation } from 'react-router-dom';
+import { Redirect, Switch, useLocation } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
 import ScrollReveal from './utils/ScrollReveal';
 import ReactGA from 'react-ga';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-// Layouts
 import LayoutDefault from './layouts/LayoutDefault';
 import AppLayout from './layouts/AppLayout';
 import Admin from './layouts/Admin';
-// Views 
 import Home from './views/Home';
-import Dashboard from './views/Dashboard';
+import Dashboard from './views/Dashboard/Dashboard';
+import UserProfile from './views/UserProfile/UserProfile';
+import TableList from './views/TableList/TableList';
+import Typography from './views/Typography/Typography';
+import Maps from './views/Maps/Maps';
+import Icons from './views/Icons/Icons';
+import NotificationsPage from './views/Notifications/Notifications';
 // Initialize Google Analytics
 ReactGA.initialize(process.env.REACT_APP_GA_CODE);
 
@@ -37,7 +41,15 @@ const App = () => {
       children={() => (
         <Switch>
           <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
-          <AppRoute exact path="/dashboard" component={Admin} />
+          <AppRoute exact path="/admin/dashboard" component={Dashboard} layout={Admin}/>
+          <AppRoute exact path="/admin/user" component={UserProfile} layout={Admin} />
+          <AppRoute exact path="/admin/table" component={TableList} layout={Admin} />
+          <AppRoute exact path="/admin/typography" component={Typography} layout={Admin} />
+          <AppRoute exact path="/admin/icons" component={Icons} layout={Admin} />
+          <AppRoute exact path="/admin/maps" component={Maps} layout={Admin} />
+          <AppRoute exact path="/admin/notifications" component={NotificationsPage} layout={Admin} />
+
+          {/* <Redirect from="/" to="/admin/dashboard"/> */}
         </Switch>
       )} />
   );
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,10 +1,7 @@
 import React from "react";
-// react plugin for creating charts
 import ChartistGraph from "react-chartist";
-// @material-ui/core
 import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
 import Store from "@material-ui/icons/Store";
 import Warning from "@material-ui/icons/Warning";
 import DateRange from "@material-ui/icons/DateRange";
@@ -16,7 +13,6 @@ import Accessibility from "@material-ui/icons/Accessibility";
 import BugReport from "@material-ui/icons/BugReport";
 import Code from "@material-ui/icons/Code";
 import Cloud from "@material-ui/icons/Cloud";
-// core components
 import GridItem from "../../components/Grid/GridItem.js";
 import GridContainer from "../../components/Grid/GridContainer.js";
 import Table from "../../components/Table/Table.js";
@@ -28,15 +24,12 @@ import CardHeader from "../../components/Card/CardHeader.js";
 import CardIcon from "../../components/Card/CardIcon.js";
 import CardBody from "../../components/Card/CardBody.js";
 import CardFooter from "../../components/Card/CardFooter.js";
-
 import { bugs, website, server } from "../../variables/general.js";
-
 import {
   dailySalesChart,
   emailsSubscriptionChart,
   completedTasksChart
 } from "../../variables/charts.js";
-
 import styles from "../../assets/jss/material-dashboard-react/views/dashboardStyle.js";
 
 const useStyles = makeStyles(styles);
@@ -44,6 +37,7 @@ const useStyles = makeStyles(styles);
 export default function Dashboard() {
   const classes = useStyles();
   return (
+    
     <div>
       <GridContainer>
         <GridItem xs={12} sm={6} md={3}>
