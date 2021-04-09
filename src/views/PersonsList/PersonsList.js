739bc4f70e3501cf6965916c7690383ccf97c407
brilingas <lauris652@gmail.com>
2021-04-09 21:37:38 +0300

    Fixing Persons table

--- src/views/PersonsList/PersonsList.js
+++ src/views/PersonsList/PersonsList.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import classnames from "classnames";
 import GridItem from "../../components/Grid/GridItem.js";
 import GridContainer from "../../components/Grid/GridContainer.js";
 import Table from "../../components/Table/Table.js";
 import Card from "../../components/Card/Card.js";
 import CardHeader from "../../components/Card/CardHeader.js";
 import CardBody from "../../components/Card/CardBody.js";
-
+import Checkbox from "@material-ui/core/Checkbox";
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+import TableRow from "@material-ui/core/TableRow";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import Edit from "@material-ui/icons/Edit";
+import Close from "@material-ui/icons/Close";
+import Check from "@material-ui/icons/Check";
 const styles = {
   cardCategoryWhite: {
     "&,& a,& a:hover,& a:focus": {
@@ -37,31 +46,82 @@ const styles = {
   }
 };
 const useStyles = makeStyles(styles);
-export default function PersonsList() {
+
+export default function PersonsList(props) {
   const classes = useStyles();
+  const [checked, setChecked] = React.useState([...props.checkedIndexes]);
+  const handleToggle = value => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+    setChecked(newChecked);
+  };
+  const { personsIndexes, persons} = props;
+  const tableCellClasses = classnames(classes.tableCell, {
+    [classes.tableCellRTL]: rtlActive
+  });
   return (
-    <GridContainer>
-      <GridItem xs={12} sm={12} md={12}>
-        <Card>
-          <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}>Persons</h4>
-          </CardHeader>
-          <CardBody>
-            <Table
-              tableHeaderColor="primary"
-              tableHead={["Name", "Surname", "Birthday", "Phone Number", "Email", "Photo", "Signature", "Address", "Gender"]}
-              tableData={[
-                ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
-                ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
-                ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
-                ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"],
-                ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"],
-                ["Mason Porter", "Chile", "Gloucester", "$78,615"]
-              ]}
-            />
-          </CardBody>
-        </Card>
-      </GridItem>
-    </GridContainer>
+    <Table className={classes.table}>
+      <TableBody>
+        {personsIndexes.map(value => (
+          <TableRow key={value} className={classes.tableRow}>
+            <TableCell className={tableCellClasses}>
+              <Checkbox
+                checked={checked.indexOf(value) !== -1}
+                tabIndex={-1}
+                onClick={() => handleToggle(value)}
+                checkedIcon={<Check className={classes.checkedIcon} />}
+                icon={<Check className={classes.uncheckedIcon} />}
+                classes={{
+                  checked: classes.checked,
+                  root: classes.root
+                }}
+              />
+            </TableCell>
+            <TableCell className={tableCellClasses}>{persons[value]}</TableCell>
+            <TableCell className={classes.tableActions}>
+              <Tooltip
+                id="tooltip-top"
+                title="Edit Task"
+                placement="top"
+                classes={{ tooltip: classes.tooltip }}
+                >
+                <IconButton
+                  aria-label="Edit"
+                  className={classes.tableActionButton}
+                >
+                  <Edit
+                    className={
+                      classes.tableActionButtonIcon + " " + classes.edit
+                    }
+                  />
+                </IconButton>
+              </Tooltip>
+              <Tooltip
+                id="tooltip-top-start"
+                title="Remove"
+                placement="top"
+                classes={{ tooltip: classes.tooltip }}
+              >
+                <IconButton
+                  aria-label="Close"
+                  className={classes.tableActionButton}
+                >
+                  <Close
+                    className={
+                      classes.tableActionButtonIcon + " " + classes.close
+                    }
+                  />
+                </IconButton>
+              </Tooltip>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
